b84c941199da140c34a58c0e49047f8ac8ead63d
Joao Santos <lourencosjm41@gmail.com>
2019-06-20 10:29:07 -0300

    alterações

--- script/escrever.js
+++ script/escrever.js
@@ -1,7 +1,8 @@
 let data =  new Date();
-
+var img = '';
 document.querySelector("#btn-salvar").addEventListener('click',function(){
     setarDados(document.querySelector(".index-form"));
+    imageHandler;
 });
 
 
@@ -19,14 +20,13 @@ function inicializaDB(){
 
 function setarDados(dado){
     let dataBase =  inicializaDB();
-    console.log(dado[3].value);
     let dados = 
     {
         nome      : dado[0].value,
         data      : data.getDate() +"/"+(data.getMonth()+1)+"/"+ data.getFullYear(),
         titulo    : dado[1].value,
         categoria : parseInt(dado[2].value),
-        imagem    : dado[3].value,
+        imagem    : img,
         conteudo  : dado[4].value,
         curtidas  : 0    
     }
@@ -35,6 +35,24 @@ function setarDados(dado){
         dataBase.assuntos.push(dados);
         localStorage.setItem("DataBase",JSON.stringify(dataBase));
     }    
+
+
 }
 
+
+function imageHandler(e2) {    
+    img = (e2.target.result );
+}
+
+function loadimage(e1) {
+    var filename = e1.target.files[0];
+    var fr = new FileReader();    
+    fr.onload = imageHandler;
+    fr.readAsDataURL(filename);
+}
+
+window.onload = function () {
+    var y = document.getElementById("get-image");
+    y.addEventListener('change', loadimage, false);
+}
 
